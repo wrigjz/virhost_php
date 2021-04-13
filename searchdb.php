51053132e1beea10ca2667739547e8c89d3bb0a3
Jon Wright <jwright.taipei@gmail.com>
2021-04-13 11:05:21 +0800

    Changed DB to UTF-8

--- searchdb.php
+++ searchdb.php
@@ -10,15 +10,16 @@
 # Retrieve the codes - check that we have ones
 $input1 = ($_POST["VIRUSNAME"]);
 
-# A test input
+# A test input and check if we were given any inputs
 #$input1 = "herpes";
 if ($input1 != true ) {
     echo "Sorry but you do not appear to have entered a recgonizable Virus name, please try again";
     exit();
 }
 
-# Now access the DB and get the data we want back from it, we use PHP mative SQLite commands
-# we have to do this twice becasue the test later on takes the 1st result that comes back if
+# Now access the DB and get the data we want back from it, we use PHP native SQLite commands
+# we have to do this once to test if there is any output and then rewind the DB to start again
+# to get the actual output if there is any becasue the test takes the 1st result that comes back if
 # it is positive
 $db = new SQLite3('/var/www/html/virhost/scripts/virus.db');
 $outtop = shell_exec('cat /var/www/html/virhost/scripts/results_top.html');
@@ -28,7 +29,7 @@ $virsearch = preg_replace("/\s+/", "", $input1); # Strip spaces etc out
 $virsearch = "%" . $virsearch . "%";
 $results = $db->query("select virusname,attachment,receptor,tissue,receptor_host,reference,pubmedid from virhost where virusname like '$virsearch'");
 
-### At this point we should have the data from the DB so we can not test the results and print them
+### At this point we should have the data from the DB so we can now test the results and print them
 # See if there are any results and if so print out any results we may have found so far
 $test = $results->fetchArray();   # Check if we found anything
 if ($test == false) {
@@ -37,8 +38,7 @@ if ($test == false) {
 else {
     echo '<hr style="border-style: solid; color: black;">';
     echo "<table>";
-    #echo "<tr><th>Virus Name</th><th>Attachment</th><th>Receptor</th><th>Tissue</th><th>Receptor Host</th></th><th>References</th><th>PubMed</th></tr>"; 
-    echo "<tr><th>Virus Name</th><th>Attachment</th><th>Receptor</th><th>Tissue</th><th>Receptor Host</th></th><th>PubMed</th></tr>"; 
+    echo "<tr><th>Virus Name</th><th>Attachment</th><th>Receptor</th><th>Tissue</th><th>Receptor Host</th></th><th>References</th><th>PubMed</th></tr>"; 
     $results->reset(); # Need to rewind the results back adter the above test
     while ($row = $results->fetchArray()) {
         echo "<tr><td style=\"text-align: center; vertical-align: middle;\">";
@@ -51,8 +51,8 @@ else {
         print "{$row['tissue']}";
         echo "</td><td style=\"text-align: center; vertical-align: middle;\">";
         print "{$row['receptor_host']}";
-        #echo "</td><td style=\"text-align: center; vertical-align: middle;\">";
-        #print "{$row['reference']}";
+        echo "</td><td style=\"text-align: center; vertical-align: middle;\">";
+        print "{$row['reference']}";
         echo "</td><td style=\"text-align: center; vertical-align: middle;\">";
         $temp1 = explode(" ", $row['pubmedid']);  # get the pubmed records from the database
         foreach ($temp1 as &$value) { # check each word to see if we print it
@@ -70,9 +70,9 @@ else {
 }
 # this is how we created the DB
 #% sqlite3 virus.db
-#sqlite> create table virhost (virusname text, attachment text, receptor text, tissue text, receptor_host text, reference text, pubmedid text);
+#sqlite> create table virhost (virusname text, attachment text, receptor text, tissue text, receptor_host text, reference, pubmedid text);
 #sqlite> .separator "^"
-#sqlite> .import viruses_table_combined.txt virhost
+#sqlite> .import viruses_table_combined.csv virhost
 #sqlite> .separator " "
 #sqlite> select virusname from virhost where virusname="Aichi virus";
 #sqlite> select virusname from virhost where virusname like '%bat%';
